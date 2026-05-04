2d94b4dded9ea90fdd6ccf0e6ecd0bb2524c9166
Alexandre Bastos <silva.bastos@gmail.com>
2026-05-03 22:38:01 -0300

    fix(azure): use node to run prisma generate to bypass permission denied error

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Iniciando processo de boot na Azure..."
 
-# Gerar o cliente Prisma dentro do container da Azure
-echo "Gerando cliente Prisma..."
-npx prisma generate
+# Gerar o cliente Prisma usando o Node diretamente para evitar erro de permissão
+echo "Gerando cliente Prisma via Node..."
+node node_modules/prisma/build/index.js generate
 
 # Iniciar o servidor Express
 echo "Iniciando servidor Node..."
