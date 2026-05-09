b8db959c31a3e3072b5a8fd9de2ea2e5e717a5b5
Alexandre Bastos <silva.bastos@gmail.com>
2026-05-09 12:51:06 -0300

    UI: change map tiles to cleaner style to remove topographic icons

--- src/components/NeighborhoodMap.tsx
+++ src/components/NeighborhoodMap.tsx
@@ -75,8 +75,8 @@ const NeighborhoodMap: React.FC<Props> = ({ cells, members }) => {
       >
         <ChangeView center={center} zoom={zoom} />
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         
         {/* Marcadores de Células */}
